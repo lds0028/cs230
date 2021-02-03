c66f417882c6651ed0749e1727e1134e1f53afe9
lds0028 <lds0028@mix.wvu.edu>
2021-02-02 21:05:35 -0500

    Lab 2 about page

---
+++ index.php
@@ -0,0 +1,50 @@
+<main>
+    <link rel="stylesheet" href="css/about.css">
+
+    <div class="about">
+        <h1>About Us</h1>
+        <p>Random</p>
+    </div>
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="images/1.jpg">
+                <div class="container">
+                    <h2>1</h2>
+                    <p style="color: red">Q*Bert</p>
+                    <p>Email: q*bert@gottlieb.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/2.jpeg">
+                <div class="container">
+                    <h2>2</h2>
+                    <p class="c1">Slick</p>
+                    <p>Email: slick@gottlieb.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/3.jpeg">
+                <div class="container">
+                    <h2>3</h2>
+                    <p>Sam</p>
+                    <p>Email: sam@gottlieb.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/4.jpeg">
+                <div class="container">
+                    <h2>4</h2>
+                    <p>Coily</p>
+                    <p>Email: coily@gottlieb.com</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
