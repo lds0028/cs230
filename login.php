17ea944a6db4ffb513e8f03b82c99a4e59a14c90
lds0028 <lds0028@mix.wvu.edu>
2021-02-19 02:48:49 -0500

    Lab 4 login page

---
+++ login.php
@@ -0,0 +1,62 @@
+<?php
+require 'includes/header.php';
+?>
+
+<main>
+    <link rel="stylesheet" href="css/login.css">
+
+    <div class="bg-cover">
+        <div class="container">
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="d-block w-100" src="images/c1.jpg" alt="First slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="images/c2.jpg" alt="Second slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="images/c3.jpg" alt="Third slide">
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </div>
+        <div class="h-40 center-me">
+            <div class="my-auto">
+                <form class="form-signin" action="includes/login-helper.php" method="post">
+
+                    <h1 class="h3 mb-3 font-weight-normal">Login</h1>
+                    <p class="hint-text">Please login</p>
+
+                    <input type="text" class="form-control" name="uname-email" placeholder="Username/ Email" required
+                        autofocus>
+
+                    <label for="inputEmail" class="sr-only">Email address</label>
+                    <input type="email" id="inputEmail" class="form-control" name="email" placeholder="Email address"
+                        required autofocus>
+
+                    <label for="inputPassword" class="sr-only">Password</label>
+                    <input type="password" id="inputPassword" class="form-control" name="pwd" placeholder="Password"
+                        required>
+
+                    <button class="btn btn-lg btn-outline-danger btn-block" name="login-submit" type="submit">Login</button>
+                    <p class="mt-5 mb-3 text-muted">&copy; 1982-1983</p>
+                </form>
+
+            </div>
+        </div>
+    </div>
+</main>
